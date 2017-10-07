78b22824090e237f8a0e46eb9661298e44ae747e
Yuma Nemoto <nemoto1257@gmail.com>
2017-10-07 16:37:34 +0900

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,6 +22,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    # assert_not flash[:success]
+    assert is_logged_in?
   end
 end
