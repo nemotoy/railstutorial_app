5fd956b70343e9edc1b7d6e6ac14b071d3b94ab7
Yuma Nemoto <nemoto1257@gmail.com>
2017-10-21 10:54:18 +0900

    Add user microposts

---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,17 @@
+class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
+  storage :file
+  # storage :fog
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # アップロード可能な拡張子のリスト
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+end
